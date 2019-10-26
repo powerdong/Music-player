ee3389fb2c54016ff57bdb4a8b4a75d1a8d4bb03
powerdong <m19893170798@163.com>
2019-10-26 11:05:45 +0800

    开始朋友页面

---
+++ musicPlayer/src/pages/friendIndex/index.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: Lambda
+ * @Begin: 2019-10-26 10:32:15
+ * @Update: 2019-10-26 11:05:27
+ * @Update log: 更新日志
+ -->
+<template>
+  <div>朋友动态展示页面</div>
+</template>
+
+<script>
+import api from 'api'
+export default {
+  name: '',
+  created () {
+    this._getFriendInfo()
+  },
+  methods: {
+    _getFriendInfo () {
+      api.friendFn()
+        .then(res => {
+          const { data } = res
+          if (data.code === 200) {
+            console.log(data)
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
---
+++ musicPlayer/src/pages/friendIndex/public.vue
@@ -0,0 +1,46 @@
+<!--
+ * @Author: Lambda
+ * @Begin: 2019-10-26 10:49:45
+ * @Update: 2019-10-26 10:59:43
+ * @Update log: 更新日志
+ -->
+<template>
+  <div>
+    <div class="img-info">
+      <img src alt />
+    </div>
+    <div class="author">
+      <h1 class="nickname">
+        范电影
+        <span>发布：</span>
+      </h1>
+      <span class="fans">19.7万粉丝</span>
+      <h1
+        class="title"
+      >Snails近期结束了在Red Rocks的演出！这次已经是他在Red Rocks演出的第三个年头了，Snails本人激动之余也向一直以来支持自己的粉丝们表达了感谢！现场视频已上传！</h1>
+      <div class="img-content">
+        <img src="http://p4.music.126.net/t4RlWJ_te7tjmgOLXlzmqw==/109951164440668313.jpg" alt />
+        <span class="center-bofang">
+          <i class="video video-bofang1"></i>
+        </span>
+        <span class="go-count">
+          <i class="video video-bofang"></i>
+          {{item.data.playTime | setCount}}
+        </span>
+        <span class="go-time">
+          <i class="video video-yinlebofangxuanlvjiezou"></i>
+          {{item.data.durationms | setTime}}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang='less' scoped>
+</style>
